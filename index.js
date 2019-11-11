59b2f117d0124f2f45879e8cc3967482f02951ef
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2019-11-11 21:56:49 +0100

    update

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://dev:***@cluster0-r2fa9.mongodb.net:21017/videoteka", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(
+    "mongodb+srv://dev:DEV123!@cluster0-r2fa9.mongodb.net/school?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   .then(res => {
-    console.log(res);
+    // console.log(res);
   })
   .catch(err => {
     console.log(err);
   });
-const Filmovi = mongoose.model("Filmovi", {
-  ime: String,
-  godina: Date,
-  zanr: [String],
-  rezija: String,
-  oscar: Boolean,
-  akteri: [String]
+const Student = mongoose.model(
+  "student",
+  new mongoose.Schema(
+    {
+      first_name: String,
+      last_name: String,
+      average_grade: Number,
+      email: String,
+      courses: [String],
+      birthday: Date
+    },
+    {
+      collection: "students"
+    }
+  )
+);
+var s = new Student({
+  first_name: "Monika",
+  last_name: "Trajkovska",
+  average_grade: 4.5,
+  email: "monika@trajkovska.mk",
+  courses: ["kibernetika", "science"],
+  birthday: new Date("1990-11-11T20:00:00Z")
 });
-Filmovi.find((err, data) => {
+s.save(err => {
   if (err) {
-    console.log("ERROR!");
+    console.log(err);
+    return;
   }
-  console.log(data);
+  console.log("save successfully");
 });
-console.log("end!");
